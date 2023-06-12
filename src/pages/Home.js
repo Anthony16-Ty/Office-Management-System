b773bf456113241a00e310bf59a9e14b69b449aa
Tonnie <tkymeu@gmail.com>
2023-06-12 09:28:53 +0300

    restyled Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ import { SidebarData } from '../components/SidebarData';
 
 function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div>
+      <h1 style={{textAlign: "center"}}>Home</h1>
       
       <Projects />
     </div>
