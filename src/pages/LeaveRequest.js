8f379a8ad939ecbf9ba9ba49d5eeae83e68b1482
Tonnie <tkymeu@gmail.com>
2023-06-23 10:02:52 +0300

    apped errors

--- src/pages/LeaveRequest.js
+++ src/pages/LeaveRequest.js
@@ -5,7 +5,7 @@ import { Button,Modal } from 'react-bootstrap';
 
 
 
-function LeaveRequest() {
+function LeaveRequest({forms, deleteData, updateForm}) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -28,94 +28,14 @@ function LeaveRequest() {
                             <th>Staff ID</th>
                             <th>Date From</th>
                             <th>Date To</th>
-                            <th>Leave Type</th>
                             <th>Reason For Leave</th>
+                            <th>Leave Type</th>
                             <th>Actions</th>
 
                         </tr>
                     </thead>
                     <tbody>
 
-                        <tr>
-                            <td>1</td>
-                            <td>Rual Octo</td>
-                            <td>Deban Steet</td>
-                            <td>Newyork</td>
-                            <td>USA</td>
-                            <td>
-                               <a href="#" class="view" title="View" data-toggle="tooltip" style={{color:"#10ab80"}}><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{color:"red"}}><i class="material-icons">&#xE872;</i></a>
-
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Demark</td>
-                            <td>City Road.13</td>
-                            <td>Dubai</td>
-                            <td>UAE</td>
-                            <td>
-                            <a href="#" class="view" title="View" data-toggle="tooltip" style={{color:"#10ab80"}}><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{color:"red"}}><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-
-
-                        <tr>
-                            <td>3</td>
-                            <td>Richa Deba</td>
-                            <td>Ocol Str. 57</td>
-                            <td>Berlin</td>
-                            <td>Germany</td>
-                            <td>
-                            <a href="#" class="view" title="View" data-toggle="tooltip" style={{color:"#10ab80"}}><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{color:"red"}}><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>4</td>
-                            <td>James Cott</td>
-                            <td>Berut Road</td>
-                            <td>Paris</td>
-                            <td>France</td>
-                            <td>
-                            <a href="#" class="view" title="View" data-toggle="tooltip" style={{color:"#10ab80"}}><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{color:"red"}}><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-
-
-                        <tr>
-                            <td>5</td>
-                            <td>Dheraj</td>
-                            <td>Bulf Str. 57</td>
-                            <td>Delhi</td>
-                            <td>India</td>
-                            <td>
-                            <a href="#" class="view" title="View" data-toggle="tooltip" style={{color:"#10ab80"}}><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{color:"red"}}><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
-
-
-                        <tr>
-                            <td>6</td>
-                            <td>Maria James</td>
-                            <td>Obere Str. 57</td>
-                            <td>Tokyo</td>
-                            <td>Japan</td>
-                            <td>
-                            <a href="#" class="view" title="View" data-toggle="tooltip" style={{color:"#10ab80"}}><i class="material-icons">&#xE417;</i></a>
-                                <a href="#" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                                <a href="#" class="delete" title="Delete" data-toggle="tooltip" style={{color:"red"}}><i class="material-icons">&#xE872;</i></a>
-                            </td>
-                        </tr>
                     </tbody>
                 </table>
             </div>
