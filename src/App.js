ef503dc22f42b963b2b5003a1123455b9e0fd434
Tonnie <tkymeu@gmail.com>
2023-06-30 22:53:10 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Projects from './pages/Projects';
 import LeaveRequest from './pages/LeaveRequest';
 import LeaveForm from './pages/LeaveForm';
 import { LeaveType } from './pages/LeaveType';
+import LeaveReport from './pages/LeaveReport';
+import ClientForm from './pages/ClientForm';
 import Client from './pages/Client';
 import TimeSheets from './pages/TimeSheets';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Logout from './pages/Logout';
 import AdminDashboard from './components/AdminDashboard';
 import StDashboard from './components/StDashboard';
 
@@ -30,6 +33,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login onLogin={handleLogin} />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Logout />} />
         <Route
           path="/admindashboard/*"
           element={<AdminDashboard isLoggedIn={isloggedIn} isAdmin={isadmin} isStaff={isStaff} />}
@@ -43,9 +47,11 @@ function App() {
         <Route path="/staff" element={<Staff />} />
         <Route path="/timesheets" element={<TimeSheets />} />
         <Route path="/client" element={<Client />} />
+        <Route path="/client-form" element={<ClientForm />} />
         <Route path="/leave-form" element={<LeaveForm />} />
         <Route path="/leave-request" element={<LeaveRequest />} />
         <Route path="/leave-type" element={<LeaveType />} />
+        <Route path="/leave-report" element={<LeaveReport />} />
       </Routes>
     </Router>
   );
