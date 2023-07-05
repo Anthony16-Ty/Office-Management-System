f356bdae888979a1b77142f7cef6273a277d381c
Tonnie <tkymeu@gmail.com>
2023-07-05 11:38:28 +0300

    update

--- src/pages/TimeSheets.js
+++ src/pages/TimeSheets.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Table, Button, Modal, Form } from 'react-bootstrap';
 import axios from 'axios';
 
-const Timesheet = ({onUpdateSheet, timesheets, deleteData, updateSheet}) => {
+const Timesheet = ({ onUpdateSheet, timesheets, deleteData, updateSheet }) => {
   const [showModal, setShowModal] = useState(false);
   const [formData, setFormData] = useState({
     date: '',
@@ -12,27 +12,44 @@ const Timesheet = ({onUpdateSheet, timesheets, deleteData, updateSheet}) => {
     task_id: '',
   });
 
-
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      if(!formData.date || !formData.start_time || !formData.end_time || !formData.progress_details || !formData.task_id){
+      if (
+        !formData.date ||
+        !formData.start_time ||
+        !formData.end_time ||
+        !formData.progress_details ||
+        !formData.task_id
+      ) {
         console.log('Please fill out all the form fields.');
         return;
-    }
-    const response = await axios.post('https://oms-api-production-acab.up.railway.app/timesheets', formData);
-    const data = response.data
-    onUpdateSheet(data);
-    setFormData({
-      date: '',
-      start_time: '',
-      end_time: '',
-      progress_details: '',
-      task_id: '',
-    });
-    console.log(data)
+      }
+
+      const startTime = new Date(`1970-01-01T${formData.start_time}`);
+      const endTime = new Date(`1970-01-01T${formData.end_time}`);
+
+      if (endTime < startTime) {
+        console.log('End time cannot be less than start time.');
+        return;
+      }
+
+      const response = await axios.post(
+        'https://oms-api-production-acab.up.railway.app/timesheets',
+        formData
+      );
+      const data = response.data;
+      onUpdateSheet(data);
+      setFormData({
+        date: '',
+        start_time: '',
+        end_time: '',
+        progress_details: '',
+        task_id: '',
+      });
+      console.log(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
@@ -43,13 +60,15 @@ const Timesheet = ({onUpdateSheet, timesheets, deleteData, updateSheet}) => {
     });
   };
 
-
   return (
     <div className='mx-auto bg-white rounded-lg shadow-lg ml-15 px-3 py-8 pt-3'>
-      <div style={{display: "flex", justifyContent: "space-between"}}>
-        <div> <h3 className='text-green text-center'>Timesheets</h3></div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <div>
-          <Button variant="primary" onClick={() => setShowModal(true)} style={{marginTop: "10px"}}>
+          {' '}
+          <h3 className='text-green text-center'>Timesheets</h3>
+        </div>
+        <div>
+          <Button variant='primary' onClick={() => setShowModal(true)} style={{ marginTop: '10px' }}>
             Add Entry
           </Button>
         </div>
@@ -62,59 +81,47 @@ const Timesheet = ({onUpdateSheet, timesheets, deleteData, updateSheet}) => {
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formDate">
+            <Form.Group controlId='formDate'>
               <Form.Label>Date</Form.Label>
-              <Form.Control
-                type="date"
-                name="date"
-                value={formData.date}
-                onChange={handleChange}
-              />
+              <Form.Control type='date' name='date' value={formData.date} onChange={handleChange} />
             </Form.Group>
-            <Form.Group controlId="formStartTime">
+            <Form.Group controlId='formStartTime'>
               <Form.Label>Start Time</Form.Label>
               <Form.Control
-                type="time"
-                name="start_time"
+                type='time'
+                name='start_time'
                 value={formData.start_time}
                 onChange={handleChange}
               />
             </Form.Group>
-            <Form.Group controlId="formEndTime">
+            <Form.Group controlId='formEndTime'>
               <Form.Label>End Time</Form.Label>
               <Form.Control
-                type="time"
-                name="end_time"
+                type='time'
+                name='end_time'
                 value={formData.end_time}
                 onChange={handleChange}
               />
             </Form.Group>
-              <Form.Group controlId="formProgress">
+            <Form.Group controlId='formProgress'>
               <Form.Label>Progress Details</Form.Label>
               <Form.Control
-                as="textarea"
-                name="progress_details"
+                as='textarea'
+                name='progress_details'
                 value={formData.progress_details}
                 onChange={handleChange}
               />
             </Form.Group>
-            <Form.Group controlId="formtask">
+            <Form.Group controlId='formtask'>
               <Form.Label>Task ID</Form.Label>
-              <Form.Control
-                as="textarea"
-                name="task_id"
-                value={formData.task_id}
-                onChange={handleChange}
-              />
+              <Form.Control as='textarea' name='task_id' value={formData.task_id} onChange={handleChange} />
             </Form.Group>
-            <Button variant="primary" type="submit" style={{marginTop: "9px"}}>
+            <Button variant='primary' type='submit' style={{ marginTop: '9px' }}>
               Add Entry
             </Button>
           </Form>
         </Modal.Body>
       </Modal>
-
-
 
       {/* Table to display timesheet entries */}
       <Table striped bordered hover>
@@ -132,16 +139,21 @@ const Timesheet = ({onUpdateSheet, timesheets, deleteData, updateSheet}) => {
           {timesheets.map((timesheet) => (
             <tr key={timesheet.id}>
               <td>{timesheet.date}</td>
-              <td>{new Date(timesheet.start_time).toLocaleTimeString('en-US', { timeStyle: 'short' })}</td>
-              <td>{new Date(timesheet.end_time).toLocaleTimeString('en-US', { timeStyle: 'short' })}</td>
+              <td>
+                {new Date(timesheet.start_time).toLocaleTimeString('en-US', { timeStyle: 'short' })}
+              </td>
+              <td>
+                {new Date(timesheet.end_time).toLocaleTimeString('en-US', { timeStyle: 'short' })}
+              </td>
               <td>{timesheet.progress_details}</td>
               <td>{timesheet.task_id}</td>
-              <td><Button variant="danger" onClick={() => deleteData(timesheet.id)}>
-              Delete
-            </Button></td>
+              <td>
+                <Button variant='danger' onClick={() => deleteData(timesheet.id)}>
+                  Delete
+                </Button>
+              </td>
             </tr>
           ))}
-
         </tbody>
       </Table>
     </div>
