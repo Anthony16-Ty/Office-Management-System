97b1252c4669b21a2a634e92e90a94e903ab0d3d
Tonnie <tkymeu@gmail.com>
2023-07-08 15:45:03 +0300

    updates

--- src/pages/ProtectedRoutes.js
+++ src/pages/ProtectedRoutes.js
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const useAuth = () => {
     // Retrieve the user authentication status from a proper source
-    const user = { loggedIn: true }; // Replace with the actual authentication check
+    const user = { loggedIn: false }; // Replace with the actual authentication check
     return user && user.loggedIn;
 };
 
