d8b741a4f71ed04640437e4616d8b6665c2951da
Tonnie <tkymeu@gmail.com>
2023-07-08 16:24:26 +0300

    updates

--- src/pages/ProtectedRoutes.js
+++ src/pages/ProtectedRoutes.js
@@ -1,14 +1,19 @@
-import { Navigate, Outlet } from "react-router-dom";
+// import React from "react";
+import { Route, Navigate } from "react-router-dom";
 
-const useAuth = () => {
-    // Retrieve the user authentication status from a proper source
-    const user = { loggedIn: false }; // Replace with the actual authentication check
-    return user && user.loggedIn;
-};
-
-const ProtectedRoutes = () => {
-    const isAuth = useAuth();
-    return isAuth ? <Outlet /> : <Navigate to="/admindashboard" />;
+const ProtectedRoutes = ({ component: Component, isAuthenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Navigate to="/" />
+        )
+      }
+    />
+  );
 };
 
 export default ProtectedRoutes;
