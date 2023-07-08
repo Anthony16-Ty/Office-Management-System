908af7437ebf20b1bb8d6c7ff2afecfb01b66412
Tonnie <tkymeu@gmail.com>
2023-07-08 16:54:27 +0300

    updates

--- src/pages/ProtectedRoutes.js
+++ src/pages/ProtectedRoutes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
 
-const ProtectedRoutes = ({ component: Component, isLoggedIn, isadmin, isStaff, ...rest }) => {
+const ProtectedRoutes = ({ component: Component, isLoggedIn, isAdmin, isStaff, ...rest }) => {
   if (!isLoggedIn) {
     return <Navigate to="/" />;
   }
 
-  if (isadmin) {
+  if (isAdmin) {
     return <Route {...rest} element={<Component />} />;
   }
 
